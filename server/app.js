a9a895c18b43bae9d37cba1e14ac67a08ac9f282
Badreddine-Ab <badreddine@gmail.com>
2023-01-23 09:37:13 +0100

    adding the project structure

---
+++ server/app.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+app.use(cors({ origin:true, credentials:true }));
+app.use(express.json())
+
+
+app.all('*',(req,res,next) => {
+    next(new apiError(`Can't find this route: ${req.originalUrl}`,400))
+})
+
+
+const port = process.env.PORT || 8081
+const server = app.listen(port, (err)=> {
+    if(!err){
+    console.log(`the port ${port} is running`)
+    }else{
+        console.log(err)
+    }
+})
+
+// Handle errors outside express
+process.on("unhandledRejection",(err)=> {
+    console.error(`UnhandledRejection Errors : ${err.name} | ${err.message}`);
+    server.close(()=> {
+        console.error('Shutting down....')
+        process.exit(1)
+    })
+    
+})
+
+module.exports = app
