ace5d6a37cf4ba803ddc1287d5acb3c2bd14d442
amina <aminasalik012@gmail.com>
2023-01-26 14:03:58 +0100

    test unitaire de commantair

---
+++ server/Test/Commatair.js
@@ -0,0 +1,21 @@
+const supertest = require('supertest')
+module.exports=app => describe('Commatair', () => {
+  it('get', async () => {
+    const res = await supertest(app)
+      .post('/api/commentaires/get')
+      .send({
+        email:"amian",
+        Commatair:"yyghjhjyjkhjkyhky"
+      })
+    expect(res.statusCode).toEqual(200)
+  })
+  it('get', async () => {
+    const res = await supertest(app)
+      .post('/api/commentaires/get')
+      .send({
+        email:"amian",
+        Commatair:"jhfghfhg"
+      })
+    expect(res.statusCode).toEqual(400)
+  })
+})
--- server/Test/Test.test.js
+++ server/Test/Test.test.js
@@ -1,2 +1,3 @@
 const app = require('../app');
 require('./Authentificattion')(app)
+require('./Commatair')(app)
