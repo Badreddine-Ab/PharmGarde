1504f9f42539ea7ecd053f31e2df0bcd102d102c
amina <aminasalik012@gmail.com>
2023-01-26 10:09:58 +0100

    Merge remote-tracking branch 'origin' into B4-9

--- front-end/src/component/commantairs/Commante.js
+++ front-end/src/component/commantairs/Commante.js
@@ -13,7 +13,10 @@ export default function Commante() {
          
         }
        
-            getAllCommant()        
+        // useEffect(() => {
+        //   getAllCommant()
+         
+        //   }, [])   
          
 
 
