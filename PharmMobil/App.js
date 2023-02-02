144002c6b3044ac3baae665f5c32380e215bc8ad
amina <aminasalik012@gmail.com>
2023-02-02 14:24:16 +0100

    Merge remote-tracking branch 'origin' into B4-9

--- PharmMobil/App.js
+++ PharmMobil/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Picker from './src/components/Detail';
+import Picker from './src/components/interface/Detail';
+// import Picker from './src/components/AddCommante/Commante';
 
 const App = () => {
   return (
