65fbb6f91e5f82c929760272877e0f8f0869ff7d
oumaima <oumaimaabouelhaitam@gmail.com>
2023-02-09 08:58:48 +0100

    Add a search bar and modify the nav component

--- PharmMobil/src/components/interface/Map.jsx
+++ PharmMobil/src/components/interface/Map.jsx
@@ -3,16 +3,18 @@ import MapView, { Marker } from "react-native-maps";
 import { StyleSheet, View } from "react-native";
 export default function Map() {
   const [mapRegion, setMapRegion] = useState({
-    Latitude:32.243843,
-    Longitude:8.522671,
-
+    Latitude: 32.243843,
+    Longitude: 8.522671,
   });
   return (
     <>
       <View style={styles.container}>
-        <MapView style={styles.map}  region={{Latitude:32.243843,Longitude:8.522671}}>
-            <Marker coordinate={mapRegion}/>
-            </MapView>
+        <MapView
+          style={styles.map}
+          region={{ Latitude: 32.243843, Longitude: 8.522671 }}
+        >
+          <Marker coordinate={mapRegion} />
+        </MapView>
       </View>
     </>
   );
