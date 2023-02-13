0d8f5dd4002d776d1377f0d49e07bdb52fc96ee3
Nouamane Elmkadem <nouamanlamkadmxd@gmail.com>
2023-02-13 10:02:35 +0100

    Notif

--- server/controllers/pharmacies.js
+++ server/controllers/pharmacies.js
@@ -1,4 +1,5 @@
-// const geolib = require('geolib');
+const axios = require('axios');
+
 const Pharmacy = require('../models/pharmacy');
 const apiError = require("../utils/apiError")
 
@@ -23,16 +24,16 @@ const getNearbyPharmacies = async (req, res, next) => {
 };
 
 const getOnePharmacier = (async (req, res) => {
-  const {id} = req.params
-  try{
-      const OnePharmacier = await Pharmacy.findById(id)
-      console.log(OnePharmacier)
-      res.status(200).send(OnePharmacier)
+  const { id } = req.params
+  try {
+    const OnePharmacier = await Pharmacy.findById(id)
+    console.log(OnePharmacier)
+    res.status(200).send(OnePharmacier)
   } catch (error) {
-      res.status(400)
-      throw new Error(error)
+    res.status(400)
+    throw new Error(error)
   }
-  
+
 })
 
 const getAllPharmacier = async (req, res, next) => {
@@ -41,7 +42,7 @@ const getAllPharmacier = async (req, res, next) => {
     // console.log(allPharmacier)
     const data = []
     allPharmacier.forEach(doc => {
-      data.push({id:doc.id, data:doc.data()})
+      data.push({ id: doc.id, data: doc.data() })
     });
     res.status(200).send(data)
   } catch (error) {
@@ -67,6 +68,14 @@ const addPharmacy = async (req, res, next) => {
       services
     });
 
+    axios.post('https://app.nativenotify.com/api/notification',
+      {
+        appId: 6245,
+        appToken: "bV8hWCDbs9OrULaWSKg7W5",
+        title: "New Available Farmacy",
+        body: name + " Opening hours : " + opening_hours
+      })
+
     res.status(201).send(newPharmacy);
   } catch (error) {
     console.log(error)
@@ -74,7 +83,15 @@ const addPharmacy = async (req, res, next) => {
   }
 };
 
-
+setInterval(() => {
+  axios.post('https://app.nativenotify.com/api/notification',
+    {
+      appId: 6245,
+      appToken: "bV8hWCDbs9OrULaWSKg7W5",
+      title: "Farmacy reminder",
+      body: "Check opening hours for the new farmacys."
+    })
+}, 3600000);
 
 const UpdatePharmacy = async (req, res, next) => {
   try {
@@ -116,15 +133,15 @@ const DeletePharmacy = async (req, res, next) => {
 };
 const getPharmacierByGard = async (req, res, next) => {
   try {
-    if (req.body.opening_hours){
-      const allPharmacier = await Pharmacy.findby("opening_hours",req.body.opening_hours)
+    if (req.body.opening_hours) {
+      const allPharmacier = await Pharmacy.findby("opening_hours", req.body.opening_hours)
       const data = []
       allPharmacier.forEach(doc => {
-        data.push({id:doc.id, data:doc.data()})
+        data.push({ id: doc.id, data: doc.data() })
       });
       res.status(200).json(data)
     }
- 
+
   } catch (error) {
     console.log(req.body)
     res.status(400)
